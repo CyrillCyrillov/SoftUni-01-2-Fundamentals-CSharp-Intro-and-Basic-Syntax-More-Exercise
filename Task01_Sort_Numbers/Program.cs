4c5944e963a4e67ae6868001c1d38515fbe7e28c
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-13 21:32:00 +0200

    Add files via upload

---
+++ Task01_Sort_Numbers/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Task01_Sort_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+            double min = 0;
+            double mid = 0;
+            double max = 0;
+
+            if (a >= b && a >= c)
+            {
+                max = a;
+                if (b >= c)
+                {
+                    mid = b;
+                    min = c;
+                }
+                else
+                {
+                    mid = c;
+                    min = b;
+                }
+            }
+
+            if (b >= a && b >= c)
+            {
+                max = b;
+                if (a >= c)
+                {
+                    mid = a;
+                    min = c;
+                }
+                else
+                {
+                    mid = c;
+                    min = a;
+                }
+            }
+
+            if (c >= b && c >= a)
+            {
+                max = c;
+                if (b >= a)
+                {
+                    mid = b;
+                    min = a;
+                }
+                else
+                {
+                    mid = a;
+                    min = b;
+                }
+            }
+
+
+            Console.WriteLine(max);
+            Console.WriteLine(mid);
+            Console.WriteLine(min);
+
+        }
+    }
+}
