b2348aef6b6ce8c1a13b1a09e7e0fc116f2a911a
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-13 21:39:11 +0200

    Add files via upload

---
+++ Task05_Messages/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task05_Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLetters = int.Parse(Console.ReadLine());
+            string message = string.Empty;
+
+            for (int i = 1; i <= numberOfLetters; i++)
+            {
+                string nextEntry = Console.ReadLine();
+                if (nextEntry == "0")
+                    {
+                        message = message + " ";
+                    }
+                else
+                    {
+                        int digits = nextEntry.Length;
+                        int mainDigit = int.Parse(nextEntry) % 10;
+                        float offset = (mainDigit - 2) * 3;
+                        if (mainDigit > 7) offset++;
+                        float letterIndex = offset + digits - 1 + 97;
+                        message = message + (char)letterIndex;
+                    }
+            }
+            
+            Console.WriteLine(message);
+        }
+    }
+}
