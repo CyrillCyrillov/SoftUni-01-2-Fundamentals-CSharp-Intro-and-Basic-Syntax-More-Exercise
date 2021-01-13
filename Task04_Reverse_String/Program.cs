a6b55c2d26d846a08e2dc220582e3055baf94061
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-13 21:35:32 +0200

    Add files via upload

---
+++ Task04_Reverse_String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task04_Reverse_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string incomeString = Console.ReadLine();
+
+            for (int i = incomeString.Length - 1; i >= 0; i--)
+            {
+                Console.Write(incomeString[i]);
+            }
+        }
+    }
+}
